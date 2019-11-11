cb246ea63820ada6a35cdcc2ae1e020a583851da
Unknown <dkmitt@sbcglobal.net>
2019-11-10 22:01:33 -0600

    initial page layout

---
+++ index.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>MyInvDB</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css\style.css">
+    <link rel="icon" type="image/png" href="images/favicon.png">
+    </head>
+    <body>
+
+        <!-- Top Navigation & Logo -->
+        <?php include 'includes/top_nav.html';?>
+        <!-- Top Navigation & Logo End -->
+
+    <!-- Page Content -->
+    <div class="container">
+        <div class="card border-0 shadow my-5">
+        <div class="card-body p-5">
+            <h1 class="font-weight-light">MyInvDB</h1>
+            <hr>
+            <p class="lead">A Simple Inventory App</p>
+            <p class="lead">more to come</p>
+            <div style="height: 300px"></div>
+            <p class="lead mb-0">You've reached the end!</p>
+            <!-- Top Navigation & Logo -->
+            <?php include 'includes/bottom_nav.html';?>
+            <!-- Top Navigation & Logo End -->
+            <hr>
+            <!-- Copyright Starts -->
+            <?php include 'includes/copyright.php';?>
+            <!-- Copyright End -->
+
+        </div>
+        </div>
+    </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
