1d257036b729d0c83eee8527c10e42352d84a584
Unknown <dkmitt@sbcglobal.net>
2019-11-16 20:16:42 -0600

    created and added seeds file

---
+++ db/seeds.sql
@@ -0,0 +1,23 @@
+INSERT INTO items (partid, item, description, manufacturer, price, qty, stockstatus) 
+VALUES ('3046', 'Adafruit Feather HUZZAH with ESP8266', 'Adafruit Feather HUZZAH with ESP8266 with Wifi', 'Adafruit', '18.95', '2','1');
+
+INSERT INTO items (partid, item, description, manufacturer, price, qty, stockstatus)  
+VALUES ('3591', 'Adafruit HUZZAH32 – ESP32 Feather Board', 'Adafruit HUZZAH32 – ESP32 Feather Board with Wifi and Bluetooth', 'Adafruit', '20.95', '2','1');
+
+INSERT INTO items (partid, item, description, manufacturer, price, qty, stockstatus)  
+VALUES ('3898', 'Lithium Ion Polymer Battery - 3.7V 400mAh', 'Lithium Ion Polymer Battery Ideal For Feathers - 3.7V 400mAh', 'Adafruit', '6.95', '1','1');
+
+INSERT INTO items (partid, item, description, manufacturer, price, qty, stockstatus)  
+VALUES ('326A', 'Blue & Yellow OLED Monochrome 0.96" 128x64 graphic display', 'Blue & Yellow Monochrome 0.96" 128x64 OLED graphic display, single color: Blue & Yellow ', 'Adafruit', '22.50', '1','1');
+
+INSERT INTO items (partid, item, description, manufacturer, price, qty, stockstatus)  
+VALUES ('326', 'Monochrome 0.96" 128x64 OLED graphic display', 'Monochrome 0.96" 128x64 OLED graphic display, single color: Blue ', 'Adafruit', '19.50', '1','1');
+
+INSERT INTO items (partid, item, description, manufacturer, price, qty, stockstatus)  
+VALUES ('3686', 'PM2.5 Air Quality Sensor and Breadboard Adapter Kit - PMS5003', 'PM2.5 Air Quality Sensor and Breadboard Adapter Kit - PMS5003', 'Adafruit', '39.95', '1','1');
+
+INSERT INTO items (partid, item, description, manufacturer, price, qty, stockstatus)  
+VALUES ('189', 'PIR (motion) sensor', 'PIR (motion) sensor', 'Adafruit', '9.95', '2','1');
+
+INSERT INTO items (partid, item, description, manufacturer, price, qty, stockstatus)  
+VALUES ('4007', 'Ultrasonic Distance Sensor - 3V or 5V - HC-SR04 compatible - RCWL-1601', 'Ultrasonic Distance Sensor - 3V or 5V - HC-SR04 compatible - RCWL-1601', 'Adafruit', '3.95', '1','1');
