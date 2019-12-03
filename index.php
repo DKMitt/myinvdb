635cfb3f8ae144ba6b87cc83950bac65722de4ca
Unknown <dkmitt@sbcglobal.net>
2019-12-02 23:53:31 -0600

    changed font awesome to be local

--- index.php
+++ index.php
@@ -7,12 +7,10 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <!-- Font Awesome CDN -->
-  <!-- <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous"> -->
-  <script src="https://kit.fontawesome.com/3a916f2625.js" crossorigin="anonymous"></script>
+  <link rel="stylesheet" type="text/css" href="css/all.css">
   <!-- Bootstrap CDN-->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
-  <link rel="stylesheet" type="text/css" href="css\style.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
   <link rel="icon" type="image/png" href="images/favicon.ico">
 </head>
 
